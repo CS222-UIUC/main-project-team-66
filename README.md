5819713f9fc48c8bb197dd976d901dd607c7f229
Somya <somyas3@illinois.edu>
2024-10-14 15:09:01 -0500

    Connected to MongoDB server

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ To start the front-end server:
 To start the back-end server: npm run dev:
 1) Navigate to server folder
 2) npm run dev
+3) MONGO_URI = mongodb+srv://somyarkp:sgSYvv38kegaqOK6@cluster0.cet2j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const corsOptions = {
+const dotenv = require("dotenv");
+const { connectDB } = require("./db/connectDB");
+
+
+// loading in the environment variables 
+dotenv.config()
+
+const app = express();
+const corsOptions = { 
     origin: ("http://localhost:5173"),
 };
 
@@ -12,5 +19,6 @@ app.get("/api", (req,res) => {
 });
 
 app.listen(8080, () => {
+    connectDB();
     console.log("server started on port 8080");
 });
