e1a28d53d43535d694bc833e1670529293e8b5e3
Khushi Patel <kpatel041720@gmail.com>
2024-10-31 19:48:24 -0500

    passed navbar test

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -15,8 +15,8 @@ function Navbar() {
   }
   return (
     <nav className='nav'>
-        <ul className='sidebar'>
-          <li onClick={hideSideBar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#000000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+        <ul className='sidebar' style={{ display: 'none' }}>
+          <li className='icon' aria-label="icon" onClick={hideSideBar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#000000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
           <li><a href="#">Browse</a></li>
           <li><a href="#">Cart</a></li>
           <li><a href="#">Seller</a></li>
@@ -26,7 +26,7 @@ function Navbar() {
           <li className='hideOnMobile'><a href="#">Browse</a></li>
           <li className='hideOnMobile'><a href="#">Cart</a></li>
           <li className='hideOnMobile'><a href="#">Seller</a></li>
-          <li className='menu-button' onClick={showSideBar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#000000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+          <li className='menu-button' aria-label="menu-button" onClick={showSideBar}><a href=""><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#000000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
         </ul>
     </nav>
   )
--- client/tests/navbar.test.js
+++ client/tests/navbar.test.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Navbar from '../src/pages/Navbar';
+import '@testing-library/jest-dom';
 
 describe('Navbar', () => {
-  test('should show and hide sidebar', () => {
+  beforeEach(() => {
     render(<Navbar />);
-    
+  });
+
+  test('should show and hide sidebar', () => {
     const sidebar = screen.getByRole('navigation').querySelector('.sidebar');
 
-    expect(sidebar).toHaveStyle('display: flex');
-
-    const hideButton = screen.getByRole('listitem', { name: /Seller/i });
-    fireEvent.click(hideButton);
-    
+    // Initially, sidebar should be visible
     expect(sidebar).toHaveStyle('display: none');
 
-    const showButton = screen.getByRole('listitem', { name: /menu-button/i });
+    // Click the hide button
+    const showButton = screen.getByLabelText(/menu-button/i);
     fireEvent.click(showButton);
 
+
+    // // Sidebar should be hidden
     expect(sidebar).toHaveStyle('display: flex');
+
+    // Click the show button
+    const hideButton = screen.getByLabelText(/icon/i);
+    fireEvent.click(hideButton);
+
+    // Sidebar should be visible again
+    expect(sidebar).toHaveStyle('display: none');
   });
 });
