f385448da5528b8092e94d5c3619d329413663d6
Khushi Patel <kpatel041720@gmail.com>
2024-11-04 09:57:59 -0600

    fixed authContext

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,17 +1,15 @@
-const { toast } = require('react-toastify');
+import { toast } from 'react-toastify';
 
 // Error popups that appear when issues with registration or login
 
-const handleSuccess = (msg)=> {
+export const handleSuccess = (msg)=> {
     toast.success(msg, {
         position: 'top-right'
     })
 }
 
-const handleError = (msg)=> {
+export const handleError = (msg)=> {
     toast.error(msg, {
         position: 'top-right'
     })
 }
-
-module.exports = { handleSuccess, handleError };
