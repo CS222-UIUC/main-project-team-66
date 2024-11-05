9d3a2550f5f11905ca06a8d4fec177901b610ffd
Somya <somyas3@illinois.edu>
2024-11-04 18:25:22 -0600

    Displaying 5 most recent items on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,26 @@ import React, {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import { handleSuccess } from '../utils';
 import {ToastContainer} from 'react-toastify'
+import axios from "axios";
 
 function Home() {
   const [loggedInUser, setLoggedInUser] = useState('');
+  const [items, setItems] = useState([]);
   const navigate = useNavigate();
 
   useEffect(()=>{
     setLoggedInUser(localStorage.getItem('loggedInUser'))
+    const fetchItems = async () => {
+      try {
+        console.log("Inside fetch items");
+        const response = await axios.get('http://localhost:8080/items/getitems'); 
+        setItems(response.data.items); 
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    };
+
+    fetchItems();
   },[])
 
   // All of the logout functionality
@@ -30,6 +43,25 @@ function Home() {
   return (
     <div>
       <h1>Welcome {loggedInUser}</h1>
+      
+      <h2>Recent Items</h2>
+      {items.length === 0 ? (
+        <p>No items available</p>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item._id}>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <p>Price: ${item.price}</p>
+              <p>Category: {item.category}</p>
+              <p>Posted on: {new Date(item.createdAt).toLocaleDateString()}</p>
+              <p>User ID: {item.seller}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+
       <button onClick={handleLogout}>Logout</button>
       <button onClick={handleCreatePost}>Create Post</button>
       <ToastContainer />
---
+++ server/Controllers/GetItems.js
@@ -0,0 +1,31 @@
+const ItemModel = require('../db/item');  
+
+const getItems = async (req, res) => {
+    try {
+        console.log("Fetching the most recent 5 items from database");
+
+        let items = await ItemModel.find({}, 'title description price category images seller createdAt')
+            .sort({ createdAt: -1 }) 
+            .limit(5);   
+        console.log("Fetched items:", items);
+              
+        items = items.map(item => ({
+            ...item.toObject(),
+        }));
+
+        res.status(200).json({
+            message: "Most recent 5 items fetched successfully",
+            success: true,
+            items
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Internal Server Error",
+            success: false,
+            error
+        });
+    }
+};
+
+module.exports = {getItems};
