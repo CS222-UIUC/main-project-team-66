7c72512a77e660be3dd8b7a962d5d9fb6aad1f4e
anikat3 <anikat3@illinois.edu>
2024-11-07 16:16:22 -0600

    Created a function for fetching user items

--- server/routes/ItemRouter.js
+++ server/routes/ItemRouter.js
@@ -1,10 +1,11 @@
 const {itemValidation} = require('../Middleware/ItemValidation');
 const {createItem} = require('../Controllers/ItemCreate');
 const {getItems} = require('../Controllers/GetItems');
-const Auth = require('../Middleware/Auth')
+const {getUserItems } = require('../Controllers/UserItems');
+const Auth = require('../Middleware/Auth');
 
 const router = require('express').Router();
 router.post('/create', Auth, itemValidation, createItem);
 router.get('/getitems', getItems);
-  
+router.get('/getuseritems', Auth, getUserItems);
 module.exports = router;
