468183c18a3acdb499f50ff86da642fdeedaab91
Somya Srivastava <145077686+somyas25@users.noreply.github.com>
2024-12-03 00:06:21 -0600

    Update Login.jsx
    Fixed error from merge

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {ToastContainer} from 'react-toastify'
-import { handleError, handleSuccess } from '../utils'
-// import illiniLogo from "client/src/assets_images/illinois_fighting_illini_logo_alternate_20141141.png";
-// import illiniLogo from "@/assets_images/illinois_fighting_illini_logo_alternate_20141141.png";
-// import campusImage from 'client/src/assets_images/drawing-rear-view-bachelor-campus-walking-after-college-building-continuous-line-art_7647-2800.jpg copy.png';
-// import 'client/src/styles/Login.css'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { handleError, handleSuccess } from '../utils';
+import illiniLogo from '../assets_images/illinois_fighting_illini_logo_alternate_20141141.png';
+import campusImage from '../assets_images/drawing-rear-view-bachelor-campus-walking-after-college-building-continuous-line-art_7647-2800.jpg copy.png';
+import '../styles/Login.css';
+import { useAuth } from '../AuthContext';
 
 function Login() {
   const [loginInfo, setLoginInfo] = useState({
